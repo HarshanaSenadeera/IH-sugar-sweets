2c6e01a61230f34a80b81dd2048a20f1b60de73b
Harshana Senadeera <116424581+HarshanaSenadeera@users.noreply.github.com>
2023-12-29 23:41:28 +0530

    Create react environment for app

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ <>
+     <h1 className="font-bold text-3xl bg-center flex justify-center hover:text-amber-700">Harshu</h1>
+ </>
+  )
+}
+
+export default App
