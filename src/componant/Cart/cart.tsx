d8abfc995f5d07d03ee711d99bdc6881ee2ee91b
Harshana Senadeera <116424581+HarshanaSenadeera@users.noreply.github.com>
2024-01-05 12:17:47 +0530

    Add cart

--- src/componant/Cart/cart.tsx
+++ src/componant/Cart/cart.tsx
@@ -10,7 +10,45 @@ export class Cart extends Component<CartProps>{
     render() {
         return (
             <>
-                <h1>This is Cart</h1>
+                <div className="flex justify-center
+                  items-center min-h-screen">
+                    <table className="w-full mx-6">
+                        <tr className="bg-gray-400">
+                            <th className="text-[9px]
+                        font-normal border-black
+                        border-[0.5px]  px-1">Id</th>
+                            <th className="text-[9px] font-normal border-black border-[0.5px] px-1">Name</th>
+                            <th className="text-[9px] font-normal border-black border-[0.5px] px-1">UnitPrice</th>
+                            <th className="text-[9px] font-normal border-black border-[0.5px] px-1">Quantity</th>
+                            <th className="text-[9px] font-normal border-black border-[0.5px] px-1">Total Price</th>
+                        </tr>
+
+                        {
+                            this.props.itemList.length === 0 ?
+                                <tr>
+                                    <td colSpan={5}
+                                        className="border-black
+                                    border-[0.5px] px-1">
+                                        <p className="text-center
+                                    text-[8px]">No Items to Display!
+                                        </p>
+                                    </td>
+                                </tr>
+                                : this.props.itemList.map((item) => (
+                                    <tr className="border-black border-[0.5px] px-1">
+                                        <td className="text-[9px]
+                                                   border-black
+                                                   border-[0.5px] px-1">
+                                            {item.product.id}</td>
+                                        <td className="text-[9px] border-black border-[0.5px] px-1">{item.product.name}</td>
+                                        <td className="text-[9px] border-black border-[0.5px] px-1">{item.product.price + ' ' + item.product.currency}</td>
+                                        <td className="text-[9px] border-black border-[0.5px] px-1">{item.itemCount}</td>
+                                        <td className="text-[9px] border-black border-[0.5px] px-1">{(item.product.price * item.itemCount) + ' ' + item.product.currency}</td>
+                                    </tr>
+                                ))
+                        }
+                    </table>
+                </div>
 
                 <a
                     href="tel:+777454885"
